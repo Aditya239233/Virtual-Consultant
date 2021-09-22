abc7c87ab001e370f12e84e2d7ab2acdd774bc50
Khush Kothari <56465845+khushk21@users.noreply.github.com>
2021-09-23 00:31:32 +0800

    Check Login and Register Functionality (#7)
    * created server
    
    * password hashing added
    
    * added test case
    
    * separated routes and controllers
    
    * added duplicate entry check
    
    * added bmi calc
    
    * scraped doctors names
    
    * refactor
    
    * login for doctor and patient added

---
+++ server/controllers/patientAccessControllers.js
@@ -0,0 +1,82 @@
+const patient = require("../model/patient");
+const bcrypt = require("bcrypt");
+
+const patientRegister = async (request, response) => {
+    console.log(response.statusCode);
+    try {
+        const patientExists = await patient.findOne({
+            email: request.body.email
+        });
+        if (patientExists) {
+            return response.status(409).send('This patient already exists');
+        }
+
+    } catch (error) {
+        response.status(400).send(error);
+    }
+    const new_patient = new patient({
+        first_name: request.body.first_name,
+        last_name: request.body.last_name,
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password,
+        age: request.body.age,
+        weight: request.body.weight,
+        height: request.body.height,
+        bmi: request.body.weight / (request.body.height * request.body.height),
+        allergies: request.body.allergies,
+        confirm_password: request.body.confirm_password
+    });
+    bcrypt.hash(new_patient.password, 10, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        new_patient.password = hash;
+        new_patient.confirm_password = hash;
+        new_patient
+            .save()
+            .then((data) => {
+                console.log("successfully created a new patient");
+            })
+            .catch((error) => {
+                console.log("error", error);
+            });
+    });
+    response.json({
+        'statuscode': response.statusCode
+    })
+}
+const patientLogin = async (request, response) => {
+    try {
+        const patient_doc = await patient.findOne({
+            username: request.body.username
+        });
+        if (patient_doc) {
+            const match = await bcrypt.compare(request.body.password, patient_doc['password']);
+            if (match) {
+                response.json({
+                    'statuscode': response.statusCode,
+                });
+            } else {
+                response.json({
+                    'statuscode': response.statusCode,
+                    'message': 'password incorrect',
+                })
+            }
+        } else {
+            response.json({
+                'statuscode': 400,
+                'message': 'user not found'
+            });
+        }
+    } catch (error) {
+        response.json({
+            'statuscode': response.statusCode
+        });
+        response.status(400).send(error)
+    }
+}
+module.exports = {
+    patientRegister,
+    patientLogin
+}
---
+++ server/model/patient.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const patientSchema = mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number
+    },
+    weight: {
+        type: Number
+    },
+    height: {
+        type: Number
+    },
+    bmi: {
+        type: String
+    },
+    allergies: {
+        type: String
+    },
+    confirm_password: {
+        type: String,
+        required: true
+    }
+});
+module.exports = mongoose.model('patient', patientSchema);
